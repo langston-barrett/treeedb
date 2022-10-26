11def67d42f70452ae9c3827f4a92634894d4fb6
Langston Barrett <langston.barrett@gmail.com>
2022-10-26 08:58:44 -0400

    treeedbgen: Complete (?) deserialization of node-types.json

--- treeedbgen/src/lib.rs
+++ treeedbgen/src/lib.rs
@@ -1,4 +1,5 @@
-// TODO(lb): Feature to use smart-string
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 /// node-types.json
@@ -7,11 +8,25 @@ pub struct Node {
     #[serde(rename(deserialize = "type", serialize = "type"))]
     pub ty: String,
     pub named: bool,
-    pub fields: Vec<Field>,
+    #[serde(default)] // empty
+    pub fields: HashMap<String, Field>,
+    #[serde(default)] // empty
+    pub subtypes: Vec<Subtype>,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Field {}
+pub struct Field {
+    multiple: bool,
+    required: bool,
+    types: Vec<Subtype>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Subtype {
+    #[serde(rename(deserialize = "type", serialize = "type"))]
+    ty: String,
+    named: bool,
+}
 
 pub fn nodes(node_types_json_str: &str) -> Result<Vec<Node>, serde_json::Error> {
     let r: Vec<Node> = serde_json::from_str(node_types_json_str)?;
