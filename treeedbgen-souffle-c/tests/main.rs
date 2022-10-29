92fb51f08564657361ba4cdc0f2a163e8957c6dd
Langston Barrett <langston.barrett@gmail.com>
2022-10-29 14:44:30 -0400

    ci: Test with Soufflé

--- treeedbgen-souffle-c/tests/main.rs
+++ treeedbgen-souffle-c/tests/main.rs
@@ -22,13 +22,12 @@ fn gen() -> Result<(), Box<dyn std::error::Error>> {
         .stdout(predicate::str::is_empty())
         .stderr(predicate::str::is_empty());
 
-    // TODO(lb): Install Soufflé in CI
-    // let mut souffle = Command::new("souffle");
-    // souffle.arg(tmp.path());
-    // souffle
-    //     .assert()
-    //     .success()
-    //     .stdout(predicate::str::contains("c_node\t17"))
-    //     .stderr(predicate::str::is_empty());
+    let mut souffle = Command::new("souffle");
+    souffle.arg(tmp.path());
+    souffle
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("c_node\t17"))
+        .stderr(predicate::str::is_empty());
     Ok(())
 }
