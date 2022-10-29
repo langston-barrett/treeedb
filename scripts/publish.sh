0f98f255a8f3c632e08f77e91c02e1dd13a27e79
Langston Barrett <langston.barrett@gmail.com>
2022-10-29 16:47:23 -0400

    scripts/publish: Sleep between publishing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,9 +3,14 @@
 set -e
 
 cargo publish --package treeedb
+sleep 10
 cargo publish --package treeedbgen
+sleep 10
 cargo publish --package treeedbgen-souffle
+sleep 10
 for lang in c csharp java javascript rust souffle swift; do
   cargo publish --package treeedb-${lang}
+  sleep 10
   cargo publish --package treeedbgen-souffle-${lang}
+  sleep 10
 done
