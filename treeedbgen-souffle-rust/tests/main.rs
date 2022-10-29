b3e05db16c9f0157ca0000c051e05ba102c33ed8
Langston Barrett <langston.barrett@gmail.com>
2022-10-29 11:51:34 -0400

    Rust

---
+++ treeedbgen-souffle-rust/tests/main.rs
@@ -0,0 +1,35 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn gen() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("treeedbgen-souffle-rust")?;
+    let tmp = NamedTempFile::new()?;
+    cmd.arg("-o").arg(tmp.path());
+    cmd.arg("--prefix=rust").arg("--printsize");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    // TODO(lb): Test!
+    // let mut cmd = Command::cargo_bin("treeedb-rust")?;
+    // cmd.arg("tests/TODO");
+    // cmd.assert()
+    //     .success()
+    //     .stdout(predicate::str::is_empty())
+    //     .stderr(predicate::str::is_empty());
+
+    // TODO(lb): Install Soufflé in CI
+    // let mut souffle = Command::new("souffle");
+    // souffle.arg(tmp.path());
+    // souffle
+    //     .assert()
+    //     .success()
+    //     .stdout(predicate::str::contains("rust_node\tTODO"))
+    //     .stderr(predicate::str::is_empty());
+    Ok(())
+}
