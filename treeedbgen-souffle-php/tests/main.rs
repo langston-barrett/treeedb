3143f82ffe2c5fb871b1426ed8c3d2b0c4f1e6af
Juan Ignacio Cortez <juan.cortez@eclypsium.com>
2025-09-23 15:58:37 -0300

    Added php information for relations generation

---
+++ treeedbgen-souffle-php/tests/main.rs
@@ -0,0 +1,33 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn test_gen() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("treeedbgen-souffle-php")?;
+    let tmp = NamedTempFile::new()?;
+    cmd.arg("-o").arg(tmp.path());
+    cmd.arg("--prefix=c").arg("--printsize");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("treeedb-php")?;
+    cmd.arg("tests/php/hello-world.php");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    let mut souffle = Command::new("souffle");
+    souffle.arg(tmp.path());
+    souffle
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("php_node\t17"))
+        .stderr(predicate::str::is_empty());
+    Ok(())
+}
