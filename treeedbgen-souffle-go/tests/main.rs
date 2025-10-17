70992abe98b07a0f8b24521a424a07332020c5a3
Juan Ignacio Cortez <juan.cortez@eclypsium.com>
2025-10-17 10:49:43 -0300

    Add Go support (#200)

---
+++ treeedbgen-souffle-go/tests/main.rs
@@ -0,0 +1,33 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use tempfile::NamedTempFile;
+
+#[test]
+fn test_gen() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("treeedbgen-souffle-go")?;
+    let tmp = NamedTempFile::new()?;
+    cmd.arg("-o").arg(tmp.path());
+    cmd.arg("--prefix=c").arg("--printsize");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    let mut cmd = Command::cargo_bin("treeedb-go")?;
+    cmd.arg("tests/go/hello-world.go");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    let mut souffle = Command::new("souffle");
+    souffle.arg(tmp.path());
+    souffle
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("go_node\t17"))
+        .stderr(predicate::str::is_empty());
+    Ok(())
+}
